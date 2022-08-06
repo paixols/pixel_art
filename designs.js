a1db7cd169d0c4c0da04dd22f354698c7d3ed398
Paix <yellowishdev@gmail.com>
2022-08-05 21:12:46 -0400

    Created designs.js for the pixel art maker including creating the grid, adding event listeners for the colors and cleaning up the grid for every submission

--- designs.js
+++ designs.js
@@ -1,10 +1,32 @@
-// Select color input
-// Select size input
-
-// When size is submitted by the user, call makeGrid()
-
-function makeGrid() {
-
-// Your code goes here!
-
+// Color Picker
+const colorPicker = document.querySelector('#colorPicker');
+// Size Picker
+const sizePicker = document.querySelector('#sizePicker');
+sizePicker.addEventListener('submit', function(event){
+    makeGrid(event);
+});
+const rows = document.querySelector('#inputHeight');
+const columns = document.querySelector('#inputWidth');
+// Design Canvas
+const designCanvas = document.querySelector('#pixelCanvas');
+// Add color to [row,column] element
+function addColor(event) {
+    event.preventDefault();
+    event.target.style.backgroundColor = colorPicker.value;
+}
+// Create Grid
+function makeGrid(event) {
+    event.preventDefault();
+    // Clean current grid
+   while(designCanvas.firstChild) {
+    designCanvas.removeChild(designCanvas.firstChild);
+   }
+    // Create Grid w/rows & columns
+    for(let row = 0; row <= rows.value; row ++) {
+        let insertRow = designCanvas.insertRow(row);
+        for(let column = 0; column <= columns.value; column++) {
+            let insertColumn = insertRow.insertCell(column);
+            insertColumn.addEventListener('click',addColor);
+        }
+    }
 }
